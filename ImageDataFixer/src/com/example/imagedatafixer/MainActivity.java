6370553352a0a247cb091773efce0ea244bbd3cd
yinglovezhuzhu <yinglovezhuzhu@139.com>
2013-07-11 15:52:04 +0800

    添加修复视频的无效数据

--- ImageDataFixer/src/com/example/imagedatafixer/MainActivity.java
+++ ImageDataFixer/src/com/example/imagedatafixer/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.imagedatafixer;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore.Images;
+import android.provider.MediaStore.Video;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,24 +23,47 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	
 	private void initView() {
 		findViewById(R.id.btn_fix_image).setOnClickListener(this);
-	}
-
-	private void fixImage() {
-		ContentResolver cr = getContentResolver();
-		int count = cr.delete(Images.Media.EXTERNAL_CONTENT_URI, "_data = ?", new String [] {"", });
-		Toast.makeText(this, getString(R.string.count) + count + getString(R.string.data), Toast.LENGTH_LONG).show();
+		findViewById(R.id.btn_fix_vedio).setOnClickListener(this);
 	}
 	
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.btn_fix_image :
-				fixImage();
+				new FixData().execute(Images.Media.EXTERNAL_CONTENT_URI);
 				break;
-
+			case R.id.btn_fix_vedio :
+				new FixData().execute(Video.Media.EXTERNAL_CONTENT_URI);
+				break;
 			default :
 				break;
 		}
 	}
 
+	
+	private class FixData extends AsyncTask<Uri, Void, Integer> {
+		
+		private ProgressDialog mmProgressDialog = null;
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mmProgressDialog = ProgressDialog.show(MainActivity.this, null, getString(R.string.fixing), true, false);
+		}
+
+		@Override
+		protected Integer doInBackground(Uri... params) {
+			ContentResolver cr = getContentResolver();
+			int count = cr.delete(params[0], "_data = ?", new String [] {"", });
+			return count;
+		}
+		
+		
+		@Override
+		protected void onPostExecute(Integer result) {
+			super.onPostExecute(result);
+			mmProgressDialog.cancel();
+			Toast.makeText(MainActivity.this, getString(R.string.count) + result + getString(R.string.data), Toast.LENGTH_LONG).show();
+		}
+	}
 }
